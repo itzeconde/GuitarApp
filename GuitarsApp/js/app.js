c989f8b54c2869d7360d64826575cde3792d728d
Itzel conde <chelconde46@gmail.com>
2025-10-13 11:49:23 -0600

    01 Load guitars

--- GuitarsApp/js/app.js
+++ GuitarsApp/js/app.js
@@ -1,50 +1,60 @@
-import { db } from "./guitarras.js"
-/*
-console.log(db [0].id)
-console.log(db [1].nombre)
-console.log(db [6].imagen)*/
+import { db } from './guitarras.js'
 
-//iterar arrays
-//ciclos
-// for(let i = 0; 1 < db.length; i++){
-// console.log(db[i].nombre)
+// console.log(db)
+// console.log('Archivo JavaScript cargado')
+// alert('Carga archivo')
+
+// console.log(db[0].id)
+// console.log(db[0].nombre)
+// console.log(db[0].imagen)
+
+// Iterar Arrays
+// Ciclos
+// for (let i = 0; i < db.length; i++) {
+//     console.log(db[i].nombre)
 // }
 
-//Metodos de Arrays para Iterar
+// Métodos de Arrays para iterar
+// db.forEach(function (guitar) {
+//     console.log(guitar)
+// })
 
-//b.forEach(
-//   function (guitar){
-//      console.log(guitar.nombre)
+// Arrow functions
+// const sayHello = (name) => {
+//     console.log('Hello ' + name)
 // }
-//)
+// sayHello('Paco')
 
-//a los div les va a insertar textos
-
-const createCard = (guitar) => {
+const createcard = (guitar) => {
     const div = document.createElement('div')
     div.className = 'col-md-6 col-lg-4 my-4 row align-items-center'
-
-    const html = `<div class="col-4">
-    <img class="img-fluid" src="./img/${guitar.imagen}.jpg" alt="imagen guitarra">
-</div>
-<div class="col-8">
-    <h3 class="text-black fs-4 fw-bold text-uppercase">${guitar.nombre}</h3>
-    <p>${guitar.descripcion}</p>
-    <p class="fw-black text-primary fs-3">${guitar.precio}</p>
-    <button 
-        type="button"
-        class="btn btn-dark w-100 "
-    >Agregar al Carrito</button>
-</div>`
+    const html = `
+        <div class="col-4">
+            <img class="img-fluid" src="./img/${guitar.imagen}.jpg" alt="imagen guitarra" />
+        </div>
+        <div class="col-8">
+            <h3 class="text-black fs-4 fw-bold text-uppercase">${guitar.nombre}</h3>
+            <p>${guitar.descripcion}</p>
+            <p class="fw-black text-primary fs-3">$${guitar.precio}</p>
+            <button type="button" class="btn btn-dark w-100" data-id="${guitar.id}">Agregar al Carrito</button>
+        </div>`
     div.innerHTML = html
     return div
 }
 
 const container = document.querySelector('main div')
 
+const buttonClicked = (e) => {
+    if (e.target.classList.contains('btn')) {
+        const dataId = e.target.getAttribute('data-id')
+        console.log(db[Number(dataId) - 1])
+    }
+}
+
+// Métodos de Arrays para iterar
 db.forEach((guitar) => {
-    console.log(guitar.nombre);
-    container.appendChild(createCard(guitar));
-});
+    console.log(guitar.nombre)
+    container.appendChild(createcard(guitar))
+})
 
-
+container.addEventListener('click', buttonClicked)
